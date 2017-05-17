d591190f1063b47a41e305beb5f6895d8263c705
André <andrelopesramos@outlook.com>
2017-05-17 18:47:25 -0300

    Http

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -5,12 +5,14 @@ import { Location }               from '@angular/common';
 
 import { Hero }         from './hero';
 import { HeroService }  from './hero.service';
+
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
+  
   hero: Hero;
 
   constructor(
@@ -23,6 +25,11 @@ export class HeroDetailComponent implements OnInit {
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
       .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void{
+    this.heroService.update(this.hero)
+      .then(()=>this.goBack());
   }
 
   goBack(): void {
