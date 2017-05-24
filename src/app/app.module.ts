7b95ce754724dafd6e86e526a2c670c8f76cc037
andrelr95 <andrelopesramos@outlook.com>
2017-05-24 15:27:56 -0300

    Completo, com correções feita do live-example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { HttpModule }     from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-// Imports para carregar e configurar  o 'in-memory web api'
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
-import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
-import { AppRoutingModule }     from './app-routing.module';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   imports: [
@@ -26,10 +28,10 @@ import { AppRoutingModule }     from './app-routing.module';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,28 +1,25 @@
-    import { Injectable }    from '@angular/core';
-    import { Headers, Http } from '@angular/http';
-    import 'rxjs/add/operator/toPromise';
-    import { Hero } from './hero';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
 
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';
-  private headers = new Headers({'Content-Type' : 'application/json'});
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred ', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private http : Http){ }
+  constructor(private http: Http) { }
 
-  getHeroes(): Promise<Hero[]>{
+  getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
                .toPromise()
                .then(response => response.json().data as Hero[])
                .catch(this.handleError);
   }
+
 
   getHero(id: number): Promise<Hero> {
     const url = `${this.heroesUrl}/${id}`;
@@ -32,28 +29,41 @@ export class HeroService {
       .catch(this.handleError);
   }
 
-  create(name: string): Promise<Hero>{
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
     return this.http
-      .post(this.heroesUrl, JSON.stringify({name: name}), {headers : this.headers})
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
       .toPromise()
       .then(res => res.json().data as Hero)
       .catch(this.handleError);
- }
+  }
 
- delete(id:number): Promise<void>{
-   const url = `${this.heroesUrl}/${id}`
-   return this.http.delete(url, {headers: this.headers})
-    .toPromise()
-    .then(() => null)
-    .catch(this.handleError);
- }
-
-  update(hero:Hero): Promise<Hero>{
-    const url = `${this.heroesUrl}/${hero.id}`
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
       .put(url, JSON.stringify(hero), {headers: this.headers})
       .toPromise()
-      .then( () => hero)
+      .then(() => hero)
       .catch(this.handleError);
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
+
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
